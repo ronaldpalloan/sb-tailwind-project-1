b859c5cfe2d8fbbb2f07844e5a096d8c13da3fc2
ronaldpalloan <rpalloan0@gmail.com>
2024-02-03 11:08:24 +0700

    add dark mode

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ hamburgerButton.addEventListener('click', function() {
 	hamburgerMenu.classList.toggle('hidden');
 	hamburgerButton.classList.toggle('open');
 })
+
+// Dark Mode
+const darkToggle = document.getElementById('dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+	if (darkToggle.checked) {
+		html.classList.add('dark');
+	} else {
+		html.classList.remove('dark');
+	}
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+	darkToggle.checked = false;
+})
+
+const body = document.querySelector('body');
+if (body.classList.contains('dark:bg-slate-800')) {
+	body.style.backgroundImage = 'none';
+}
